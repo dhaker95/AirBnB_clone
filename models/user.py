ef7ec060f4d298fab01347b62e5223b9a3010785
Dhaker Achour <achour.dhaker@gmail.com>
2023-03-11 11:51:53 -0800

    FIX

--- models/user.py
+++ models/user.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
-
 from models.base_model import BaseModel
+""" subclass User module """
 
 
-class State(BaseModel):
-    """Class for managing state objects"""
+class User(BaseModel):
+    """ subclass user that inherits from Basemodel"""
 
-    name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
