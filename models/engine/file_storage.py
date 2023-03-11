11cd2354932e9e67b02dcf6734f49ea39ec348aa
Dhaker Achour <achour.dhaker@gmail.com>
2023-03-11 11:53:13 -0800

    FIX

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,95 +1,69 @@
 #!/usr/bin/python3
-"""Module for FileStorage class."""
-import datetime
+""" File storage module """
 import json
-import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import models
 
 
 class FileStorage:
+    """ class FileStorage
+    Serves as file storage engine
 
-    """Class for storing and retrieving data"""
+    attrs:
+        __file path (str): Name of file path to save object to
+        __objects (dict): Dictionary of instantiated keyword argument
+
+    """
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """  returns the dictionary __objects """
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
+        """ sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, str(obj.id))
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-            json.dump(d, f)
+        """ serializes __objects to the JSON file """
+        to_dict = {}
+        for key, obj in FileStorage.__objects.items():
+            to_dict[key] = obj.to_dict()
 
-    def classes(self):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
+        with open(FileStorage.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(to_dict, f)
 
     def reload(self):
-        """Reloads the stored objects"""
-        if not os.path.isfile(FileStorage.__file_path):
+        """ deserializes the JSON file to __objects """
+        try:
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
+                load = json.load(f)
+                for key, value in load.items():
+                    FileStorage.__objects[key] = eval(
+                        value['__class__'])(**value)
+        except FileNotFoundError:
             return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
-            FileStorage.__objects = obj_dict
 
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attributes
+    def count(self, cls):
+        """
+        counts the number of objects in storage
+        """
+        counter = 0
+        for key in models.storage.all().keys():
+            if cls in key:
+                counter += 1
+        return counter
